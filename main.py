1476ac6b1cf80856722e33036158933cec5a2ef8
Adriano <adriano.garcia-giraldamilena@unisalento.studenti.it>
2022-07-14 20:06:07 +0200

    Adding the final files of recommendation system

--- main.py
+++ main.py
@@ -201,11 +201,14 @@ def obtainpredictions():
 
     #Because of linear regression wasn't behaving well with hours as variable we decided to create the functions from the average
 
+    # q3 = """SELECT BatteryPower,HOUR(timestamp) FROM grafana.battery where Id = (SELECT LAST_INSERT_ID()) FROM grafana.battery)"""
+    #q3 = """SELECT BatteryPower,HOUR(timestamp) FROM grafana.battery where timestamp= (SELECT MAX(timestamp) FROM grafana.battery)"""
+    q3 = """SELECT BatteryPower,HOUR(timestamp) FROM grafana.battery """
 
-    q3 = """SELECT BatteryPower,HOUR(timestamp) FROM grafana.battery where timestamp= (SELECT MAX(timestamp) FROM grafana.battery)"""
-    results = read_query(cdbconnection, q3)
+
+    resul = read_query(cdbconnection, q3)
+    results=resul[len(resul)-1]
     print("Last battery charge"+str(results))
-
     #We have to give it manually the wheights one important thing should be store the day weather in orther to future modelization
     #So we can obtain the true values of the weather
     WeatherFactor = 0
@@ -231,8 +234,10 @@ def obtainpredictions():
     #SUBSTRACTING THE AVG PANNEL & AVG CONSUMPTION TO WHITH BATTERY TO BATTERY PREDICTION:
     voltaje = 3.6
     maxCAP = 4000
-    hora=int(results[0][1])
-    op = float(results[0][0])
+    hora=int(results[1])
+    op = float(results[0])
+    print(hora)
+    print(op)
     # PREDICTION OF BATTERY STATUS IN THE NEXT 24 HOUR
 
     for i in range(hora, hora + 24):
@@ -251,7 +256,7 @@ def obtainpredictions():
     print("Expected Consumption: " + str(prediction))
 
     Lights = "Off"
-    TurnonTurnoffinterval = [0,23]  # So if the hours are in this interval it will turn on for try purposes we are letting it as 12 23
+    TurnonTurnoffinterval = [12,23]  # So if the hours are in this interval it will turn on for try purposes we are letting it as 12 23
     # if Battery % >20 && TurnonTurnoff Lights ON
     if (predictionbat[4] / maxCAP) > 0.1 and hora >= TurnonTurnoffinterval[0] and hora <= TurnonTurnoffinterval[1]:
         Lights = "On"
@@ -274,7 +279,7 @@ def obtainpredictions():
     print("TEMP"+str(results))
     if  temp<12:
         Temp="TemperatureThe house has a cold temperature we suggest to put the heating if you are at home:"+str(temp)
-    if  temp >=34 and temp<42:
+    if  temp >=25 and temp<30:
         Temp="Temperature The house has an very high temperature we reccomend to put the AC: "+str(temp)
     if temp >= 34 and temp < 42:
         Temp = "Temperature ALERT: The house has an anormally high temperature mind about a possible fire:"+str(temp)
@@ -300,16 +305,17 @@ def obtainpredictions():
 
 
 mqtt.Client.connected_flag=False#create flag in class
-broker="mqtt.eclipseprojects.io"
-client = mqtt.Client("mqttjs03")             #create new instance
+broker="20.216.178.106"
+port = 1883
+client = mqtt.Client()             #create new instance
 client.on_connect=on_connect  #bind call back function
 client.loop_start()
 print("Connecting to broker ",broker)
-client.connect(broker)      #connect to broker
+client.connect(broker,port)      #connect to broker
 while not client.connected_flag: #wait in loop
     msg=obtainpredictions() #obtains the predictions with the function above
     client.publish("unisalento/smarthome/raspberry1/actuators/leds",json.dumps(msg) )
-    time.sleep(60)
+    time.sleep(15)
 print("in Main Loop")
 client.loop_stop()    #Stop loop
 client.disconnect() # disconnect
